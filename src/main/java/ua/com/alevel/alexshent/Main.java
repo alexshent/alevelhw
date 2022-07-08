7ce441dd1ec675230fe657e0725de95dedfc83c3
Alexey Shentyakov <alexey.shentyakov@gmail.com>
2022-07-08 07:04:30 +0300

    demo classes

---
+++ src/main/java/ua/com/alevel/alexshent/Main.java
@@ -0,0 +1,11 @@
+package ua.com.alevel.alexshent;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.createProducts();
+        demo.changeBoatProduct();
+        demo.deleteBoatProduct();
+    }
+}
