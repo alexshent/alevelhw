4d1d90bb7a4d406dac337824d94d9de4c81efd61
Alexey Shentyakov <alexey.shentyakov@gmail.com>
2022-07-23 09:59:14 +0300

    Add separator line

--- src/main/java/ua/com/alevel/alexshent/Demo.java
+++ src/main/java/ua/com/alevel/alexshent/Demo.java
@@ -14,6 +14,7 @@ public class Demo {
     private final BicycleService bicycleService = new BicycleService();
     private final BoatService boatService = new BoatService();
     String targetBoatId;
+    private final String separatorLine = "------------------";
 
     public void createProducts() {
         List<Automobile> autos = autoService.createAutos(10);
@@ -32,19 +33,19 @@ public class Demo {
 
     public void changeBoatProduct() {
         Boat  boat = boatService.getProductById(targetBoatId);
-        System.out.println("------------------");
+        System.out.println(separatorLine);
         boatService.printAll();
         boat.setModel("m-m-m");
         boatService.updateProduct(boat);
-        System.out.println("------------------");
+        System.out.println(separatorLine);
         boatService.printAll();
     }
 
     public void deleteBoatProduct() {
-        System.out.println("------------------");
+        System.out.println(separatorLine);
         boatService.printAll();
         boatService.deleteProduct(targetBoatId);
-        System.out.println("------------------");
+        System.out.println(separatorLine);
         boatService.printAll();
     }
 
@@ -52,6 +53,7 @@ public class Demo {
         Container<Vehicle> container = new Container<>();
         Boat boat = new Boat("AAA", BoatManufactures.FFF, BigDecimal.valueOf(1000.00), true);
         container.addVehicle(boat);
+        System.out.println(separatorLine);
         System.out.println("vehicle class = " + container.getVehicleClassName());
         System.out.println("price = " + container.getPrice());
         System.out.println("discount price = " + container.getDiscountPrice().toString());
