a3937389b6927110b76ae513cf5dfef406e9c5b3
Alexey Shentyakov <alexey.shentyakov@gmail.com>
2022-07-23 09:44:36 +0300

    Add container class

---
+++ src/main/java/ua/com/alevel/alexshent/Container.java
@@ -0,0 +1,40 @@
+package ua.com.alevel.alexshent;
+
+import ua.com.alevel.alexshent.model.Vehicle;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+public class Container <V> {
+    private V vehicle;
+
+    public void addVehicle(V vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getVehicleClassName() {
+        return vehicle.getClass().getName();
+    }
+
+    public BigDecimal getPrice() {
+        return ((Vehicle) vehicle).getPrice();
+    }
+
+    public BigDecimal getDiscountPrice() {
+        final int discountBottom = 10;
+        final int discountTop = 30;
+        Random random = new Random();
+        int discount = random.nextInt(discountBottom, discountTop + 1);
+        BigDecimal result = ((Vehicle) vehicle).getPrice().multiply(
+                BigDecimal.valueOf((100 - discount) / 100.0)
+        );
+        return result.setScale(2, RoundingMode.CEILING);
+    }
+
+    public BigDecimal getExtraChargePrice(Number extraCharge) {
+        return  ((Vehicle) vehicle).getPrice().add(
+                BigDecimal.valueOf(extraCharge.doubleValue())
+        );
+    }
+}
--- src/main/java/ua/com/alevel/alexshent/Demo.java
+++ src/main/java/ua/com/alevel/alexshent/Demo.java
@@ -1,13 +1,12 @@
 package ua.com.alevel.alexshent;
 
-import ua.com.alevel.alexshent.model.Automobile;
-import ua.com.alevel.alexshent.model.Bicycle;
-import ua.com.alevel.alexshent.model.Boat;
+import ua.com.alevel.alexshent.model.*;
 import ua.com.alevel.alexshent.repository.Repository;
 import ua.com.alevel.alexshent.service.AutomobileService;
 import ua.com.alevel.alexshent.service.BicycleService;
 import ua.com.alevel.alexshent.service.BoatService;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class Demo {
@@ -47,5 +46,15 @@ public class Demo {
         boatService.deleteProduct(targetBoatId);
         System.out.println("------------------");
         boatService.printAll();
+    }
+
+    public void useContainer() {
+        Container<Vehicle> container = new Container<>();
+        Boat boat = new Boat("AAA", BoatManufactures.FFF, BigDecimal.valueOf(1000.00), true);
+        container.addVehicle(boat);
+        System.out.println("vehicle class = " + container.getVehicleClassName());
+        System.out.println("price = " + container.getPrice());
+        System.out.println("discount price = " + container.getDiscountPrice().toString());
+        System.out.println("extra charge price = " + container.getExtraChargePrice(100));
     }
 }
