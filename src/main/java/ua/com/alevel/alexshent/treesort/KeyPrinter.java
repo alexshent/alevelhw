aa4b8356dd5604625ed7f55ec2f7b76c05eaa219
Alexey Shentyakov <alexey.shentyakov@gmail.com>
2022-08-02 16:23:57 +0300

    Remove Java tree sort code

--- src/main/java/ua/com/alevel/alexshent/treesort/KeyPrinter.java
+++
@@ -1,7 +0,0 @@
-package ua.com.alevel.alexshent.treesort;
-
-class KeyPrinter implements TreeVisitor {
-    public void visit(Tree node) {
-        System.out.println(" " + node.key);
-    }
-}
--- src/main/java/ua/com/alevel/alexshent/treesort/TreeVisitor.java
+++
@@ -1,5 +0,0 @@
-package ua.com.alevel.alexshent.treesort;
-
-public interface TreeVisitor {
-    void visit(Tree node);
-}
