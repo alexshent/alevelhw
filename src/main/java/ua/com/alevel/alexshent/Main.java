415ae776ce3fa3528b38b57b389ad033c14f9402
Alexey Shentyakov <alexey.shentyakov@gmail.com>
2022-08-05 00:29:10 +0300

    Use Stream API

--- src/main/java/ua/com/alevel/alexshent/Main.java
+++ src/main/java/ua/com/alevel/alexshent/Main.java
@@ -1,11 +1,8 @@
 package ua.com.alevel.alexshent;
 
 public class Main {
-
     public static void main(String[] args) {
         Demo demo = new Demo();
-        demo.createProducts();
-        demo.changeBoatProduct();
-        demo.deleteBoatProduct();
+        demo.useStreams();
     }
 }
